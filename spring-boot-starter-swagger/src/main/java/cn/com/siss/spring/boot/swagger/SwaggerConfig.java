2d2307d9e9da034afcf553cfc6919cc61a762cdb
huangjun <huangjun@siss.com>
2019-12-26 17:11:31 +0800

    dispose pom dependency jar version

--- spring-boot-starter-swagger/src/main/java/cn/com/siss/spring/boot/swagger/SwaggerConfig.java
+++ spring-boot-starter-swagger/src/main/java/cn/com/siss/spring/boot/swagger/SwaggerConfig.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -54,17 +52,6 @@ public class SwaggerConfig {
                 .title(swaggerProperties.getTitle())
                 .version(swaggerProperties.getVersion())
                 .build();
-    }
-
-    @Bean
-    public WebMvcConfigurerAdapter forwardToIndex() {
-        return new WebMvcConfigurerAdapter() {
-            public void addViewControllers(ViewControllerRegistry registry) {
-                if (swaggerProperties.getRedirect()) {
-                    registry.addViewController("/").setViewName("redirect:/swagger-ui.html");
-                }
-            }
-        };
     }
 
 }
